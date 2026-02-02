bacaa5b9f72e7f6b609759dcbef280b02fa65a28
abhinay-x <abhigrok3@gmail.com>
2026-02-03 00:52:22 +0530

    fix: profile update error

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,42 +1,42 @@
 const User = require('../models/User');
 
-const asyncHandler = (fn) => async (req, res, next) => {
+const getMe = async (req, res, next) => {
   try {
-    await fn(req, res, next);
+    const user = await User.findById(req.user._id).select('_id name email');
+
+    if (!user) {
+      res.status(404);
+      throw new Error('User not found');
+    }
+
+    res.json(user);
   } catch (error) {
     next(error);
   }
 };
 
-const getMe = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.user._id).select('_id name email');
+const updateMe = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id);
 
-  if (!user) {
-    res.status(404);
-    throw new Error('User not found');
+    if (!user) {
+      res.status(404);
+      throw new Error('User not found');
+    }
+
+    user.name = req.body.name ?? user.name;
+    user.email = req.body.email ?? user.email;
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
+  } catch (error) {
+    next(error);
   }
-
-  res.json(user);
-});
-
-const updateMe = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.user._id);
-
-  if (!user) {
-    res.status(404);
-    throw new Error('User not found');
-  }
-
-  user.name = req.body.name ?? user.name;
-  user.email = req.body.email ?? user.email;
-
-  const updatedUser = await user.save();
-
-  res.json({
-    _id: updatedUser._id,
-    name: updatedUser.name,
-    email: updatedUser.email,
-  });
-});
+};
 
 module.exports = { getMe, updateMe };
